937e683dbad498da0eb18139d7c33fdd5cd2dd8c
Xaos <xaosmaos@gmail.com>
2021-09-05 22:31:56 +0300

    исправлен - try to catch

--- app/src/main/java/com/example/lucky_ticket_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/lucky_ticket_kotlin/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.lucky_ticket_kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity() {
+    private val maxLength = 6
+    private val number = findViewById<EditText>(R.id.number)
+    private val bubble = findViewById<ImageView>(R.id.bubble)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onClick(view: View) {
+
+        if (number.length() == maxLength) {
+
+            try {
+                val digits = number.text.toString()
+                val sum_a = digits.substring(0, 3).toInt()
+                val sum_b = digits.substring(3, 6).toInt()
+
+                if (sum_a == sum_b)
+                {
+                    Toast.makeText(applicationContext, R.string.yes, Toast.LENGTH_LONG).show()
+                    bubble.setImageResource(R.drawable.bubl_green)
+                }
+                 else
+                {
+                Toast.makeText(applicationContext, R.string.no, Toast.LENGTH_LONG).show()
+                bubble.setImageResource(R.drawable.bubl_red)
+            }
+        }
+        catch (e: NumberFormatException)
+        {
+            Toast.makeText(applicationContext, R.string.dig, Toast.LENGTH_LONG).show()
+    }
+}
+
+}
 }
