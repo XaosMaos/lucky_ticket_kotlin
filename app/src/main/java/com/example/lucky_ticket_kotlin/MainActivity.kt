7ad495f839499d762061404ba389f4cdae9fb0d1
Xaos <xaosmaos@gmail.com>
2021-09-06 00:11:34 +0300

    дбавлен fun onClick()

--- app/src/main/java/com/example/lucky_ticket_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/lucky_ticket_kotlin/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.lucky_ticket_kotlin
 
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
@@ -11,37 +11,33 @@ class MainActivity() : AppCompatActivity() {
     private val maxLength = 6
     private val number = findViewById<EditText>(R.id.number)
     private val bubble = findViewById<ImageView>(R.id.bubble)
+    private val button = findViewById<Button>(R.id.button)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener { onClick() }
     }
 
-    fun onClick(view: View) {
-
+    private fun onClick() {
         if (number.length() == maxLength) {
-
             try {
                 val digits = number.text.toString()
                 val sum_a = digits.substring(0, 3).toInt()
                 val sum_b = digits.substring(3, 6).toInt()
 
-                if (sum_a == sum_b)
-                {
+                if (sum_a == sum_b) {
                     Toast.makeText(applicationContext, R.string.yes, Toast.LENGTH_LONG).show()
                     bubble.setImageResource(R.drawable.bubl_green)
+                } else {
+                    Toast.makeText(applicationContext, R.string.no, Toast.LENGTH_LONG).show()
+                    bubble.setImageResource(R.drawable.bubl_red)
                 }
-                 else
-                {
-                Toast.makeText(applicationContext, R.string.no, Toast.LENGTH_LONG).show()
-                bubble.setImageResource(R.drawable.bubl_red)
+            } catch (e: NumberFormatException) {
+                Toast.makeText(applicationContext, R.string.dig, Toast.LENGTH_LONG).show()
             }
         }
-        catch (e: NumberFormatException)
-        {
-            Toast.makeText(applicationContext, R.string.dig, Toast.LENGTH_LONG).show()
-    }
-}
 
-}
+    }
 }
